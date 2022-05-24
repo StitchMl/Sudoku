dda906a050ace22ee517a7c45b8d487b0b383088
StitchMl <matteo.lagioia@gmail.com>
2022-05-24 16:57:56 +0200

    Commit of first Screen

--- app/src/main/java/com/example/sudoku/screen/FirstScreen.kt
+++ app/src/main/java/com/example/sudoku/screen/FirstScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sudoku.R
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
 
@@ -26,113 +27,119 @@ import kotlinx.coroutines.launch
 @Preview(device = Devices.DEFAULT, showBackground = true)
 @Composable
 fun FirstScreen(){
-    val context = LocalContext.current
     val scope = rememberCoroutineScope()
     val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
-    val diff: Array<String> = context.resources.getStringArray(R.array.difficulty)
     BottomDrawer(
         modifier = Modifier.background(MaterialTheme.colors.onPrimary),
         drawerState = drawerState,
         drawerContent = {
-            Column(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(stringResource(R.string.difficut), fontSize = 40.sp)
-                Column {
-                    for (state in 1 until diff.size) {
-                        Row(modifier = Modifier
-                            .clickable { }
-                            .padding(3.dp)) {
-                            Column (horizontalAlignment = Alignment.Start) {
-                                Text(text = diff[state], modifier = Modifier.padding(horizontal = 10.dp))
-                            }
-                            Column(horizontalAlignment = Alignment.Start) {
-                                Row(modifier = Modifier
-                                    .padding(3.dp)) {
-                                    for (i in 1 until state + 1) {
-                                        Icon(
-                                            imageVector = ImageVector.vectorResource(R.drawable.star),
-                                            contentDescription = null,
-                                            modifier = Modifier.size(16.dp)
-                                        )
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-                Row(modifier = Modifier
-                    .clickable {
-                        scope.launch {
-                            drawerState.apply {
-                                if (isClosed) open() else close()
-                            }
-                        }
-                    }
-                    .padding(5.dp)){
-                    Icon(imageVector = Icons.Rounded.Close, contentDescription = null)
-                }
-            }
+            DrawerContent(scope, drawerState)
         },
         gesturesEnabled = false
     ) {
-        Scaffold(
-            floatingActionButton = {
-                FloatingActionButton(onClick = { /*TODO*/ }) {
-                    Icon(ImageVector.vectorResource(R.drawable.trophy), "")
-                }
-            },
-            // Defaults to false
-            isFloatingActionButtonDocked = true
-        ) {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text(stringResource(R.string.sudoku), fontSize = 70.sp)
-                Text(stringResource(R.string.group), fontSize = 14.sp)
-                Spacer(modifier = Modifier.size(70.dp))
-                Button(
-                    onClick = {
-                        scope.launch {
-                            drawerState.apply {
-                                if (isClosed) open() else close()
+        PrincipalScreen(scope, drawerState)
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun DrawerContent(scope: CoroutineScope, drawerState: BottomDrawerState){
+    val context = LocalContext.current
+    val diff: Array<String> = context.resources.getStringArray(R.array.difficulty)
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(stringResource(R.string.difficut), fontSize = 40.sp)
+        Column {
+            for (state in 1 until diff.size) {
+                Row(modifier = Modifier
+                    .clickable { }
+                    .padding(3.dp)) {
+                    Column (horizontalAlignment = Alignment.Start) {
+                        Text(text = diff[state],
+                            modifier = Modifier.padding(horizontal = 10.dp))
+                    }
+                    Column(horizontalAlignment = Alignment.Start) {
+                        Row(modifier = Modifier.padding(3.dp)) {
+                            for (i in 1 until state + 1) {
+                                Icon(
+                                    imageVector = ImageVector.vectorResource(R.drawable.star),
+                                    contentDescription = null,
+                                    modifier = Modifier.size(16.dp)
+                                )
                             }
                         }
-                    },
-                    // Uses ButtonDefaults.ContentPadding by default
-                    contentPadding = PaddingValues(
-                        start = 20.dp,
-                        top = 12.dp,
-                        end = 20.dp,
-                        bottom = 12.dp
-                    ),
-                    modifier = Modifier
-                        .width(190.dp)
-                        .height(50.dp)
-                ) {
-                    Text(stringResource(R.string.new_game), fontSize = 22.sp)
+                    }
                 }
-                Button(
-                    onClick = {
-                        //TODO
-                    },
-                    // Uses ButtonDefaults.ContentPadding by default
-                    contentPadding = PaddingValues(
-                        start = 20.dp,
-                        top = 10.dp,
-                        end = 20.dp,
-                        bottom = 10.dp
-                    ),
-                    modifier = Modifier
-                        .padding(10.dp)
-                        .width(160.dp)
-                        .height(40.dp)
-                ) {
-                    Text(stringResource(R.string.load_game), fontSize = 13.sp)
+            }
+        }
+        Row(modifier = Modifier
+            .clickable {
+                scope.launch {
+                    drawerState.apply {
+                        if (isClosed) open() else close()
+                    }
                 }
+            }
+            .padding(5.dp)){
+            Icon(imageVector = Icons.Rounded.Close, contentDescription = null)
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Composable
+fun PrincipalScreen(scope: CoroutineScope, drawerState: BottomDrawerState){
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { /*TODO*/ }) {
+                Icon(ImageVector.vectorResource(R.drawable.trophy),
+                    "") } },
+        isFloatingActionButtonDocked = true
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(stringResource(R.string.sudoku), fontSize = 70.sp)
+            Text(stringResource(R.string.group), fontSize = 14.sp)
+            Spacer(modifier = Modifier.size(70.dp))
+            Button(
+                onClick = {
+                    scope.launch {
+                        drawerState.apply {
+                            if (isClosed) open() else close()
+                        }
+                    }
+                },
+                contentPadding = PaddingValues(
+                    start = 20.dp,
+                    top = 12.dp,
+                    end = 20.dp,
+                    bottom = 12.dp
+                ),
+                modifier = Modifier
+                    .width(190.dp)
+                    .height(50.dp)
+            ) {
+                Text(stringResource(R.string.new_game), fontSize = 22.sp)
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                contentPadding = PaddingValues(
+                    start = 20.dp,
+                    top = 10.dp,
+                    end = 20.dp,
+                    bottom = 10.dp
+                ),
+                modifier = Modifier
+                    .padding(10.dp)
+                    .width(160.dp)
+                    .height(40.dp)
+            ) {
+                Text(stringResource(R.string.load_game), fontSize = 13.sp)
             }
         }
     }
