fd5c9771f14d39a963ea7a2484f520db2377c540
StitchMl <matteo.lagioia@gmail.com>
2022-05-24 13:47:06 +0200

    Commit of first Screen

--- app/src/main/java/com/example/sudoku/screen/FirstScreen.kt
+++ app/src/main/java/com/example/sudoku/screen/FirstScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.outlined.AccountBox
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ImageBitmap
@@ -18,8 +19,9 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.sudoku.R
+import kotlinx.coroutines.launch
 
-@Preview(device = Devices.DEFAULT, showBackground = true)
+//@Preview(device = Devices.DEFAULT, showBackground = true)
 @Composable
 fun FirstScreen(){
     Scaffold(
@@ -79,6 +81,83 @@ fun FirstScreen(){
                     .height(40.dp)
             ) {
                 Text("Continua partita", fontSize = 14.sp)
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterialApi::class)
+@Preview(device = Devices.DEFAULT, showBackground = true)
+@Composable
+fun FirstScreenProva(){
+    val scope = rememberCoroutineScope()
+    val drawerState = rememberBottomDrawerState(BottomDrawerValue.Closed)
+    BottomDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            // Drawer content
+        }
+    ) {
+        Scaffold(
+            floatingActionButton = {
+                FloatingActionButton(onClick = { /* ... */ }) {
+                    Icon(ImageVector.vectorResource(R.drawable.trophy), "")
+                }
+            },
+            // Defaults to false
+            isFloatingActionButtonDocked = true
+        ) {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text("Sudoku", fontSize = 70.sp)
+                Text("Il calice di Android", fontSize = 14.sp)
+                Spacer(modifier = Modifier.size(70.dp))
+                Button(
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    },
+                    // Uses ButtonDefaults.ContentPadding by default
+                    contentPadding = PaddingValues(
+                        start = 20.dp,
+                        top = 12.dp,
+                        end = 20.dp,
+                        bottom = 12.dp
+                    ),
+                    modifier = Modifier
+                        .width(190.dp)
+                        .height(50.dp)
+                ) {
+                    Text("Nuova partita", fontSize = 22.sp)
+                }
+                Button(
+                    onClick = {
+                        scope.launch {
+                            drawerState.apply {
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    },
+                    // Uses ButtonDefaults.ContentPadding by default
+                    contentPadding = PaddingValues(
+                        start = 20.dp,
+                        top = 12.dp,
+                        end = 20.dp,
+                        bottom = 12.dp
+                    ),
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .width(160.dp)
+                        .height(40.dp)
+                ) {
+                    Text("Continua partita", fontSize = 13.sp)
+                }
             }
         }
     }
