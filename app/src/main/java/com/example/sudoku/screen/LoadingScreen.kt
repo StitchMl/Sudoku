e3004ae28ea54e85139e15eb2690baf385900b5d
StitchMl <matteo.lagioia@gmail.com>
2022-05-25 14:52:04 +0200

    Commit

--- app/src/main/java/com/example/sudoku/screen/LoadingScreen.kt
+++ app/src/main/java/com/example/sudoku/screen/LoadingScreen.kt
@@ -9,14 +9,15 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.sudoku.R
+import com.example.sudoku.ui.theme.Purple700
 import com.example.sudoku.ui.theme.SudokuEIlCaliceDiAndroidTheme
-import com.example.sudoku.ui.theme.lightGrey
 import com.example.sudoku.ui.theme.mainTitle
 
 @Preview(device = Devices.PIXEL_3)
@@ -30,7 +31,7 @@ fun LoadingPreview() {
 @Composable
 fun LoadingScreen() {
     Surface(
-        color = MaterialTheme.colors.primary,
+        color = MaterialTheme.colors.surface,
         modifier = Modifier
             .fillMaxHeight()
             .fillMaxWidth()
@@ -42,20 +43,22 @@ fun LoadingScreen() {
 
             Image(
                 painter = painterResource(R.drawable.ic_launcher_foreground),
-                modifier = Modifier.size(128.dp),
-                contentDescription = ""
+                modifier = Modifier
+                    .size(200.dp),
+                contentDescription = "",
+                contentScale = ContentScale.Fit
             )
 
             LinearProgressIndicator(
-                color = lightGrey,
+                color = Purple700,
                 modifier = Modifier
                     .width(128.dp)
-                    .padding(16.dp)
+                    .padding(bottom = 16.dp)
             )
 
             Text(
                 text = stringResource(id = R.string.sudoku),
-                style = mainTitle.copy(color = MaterialTheme.colors.secondary),
+                style = mainTitle.copy(color = MaterialTheme.colors.primary),
                 modifier = Modifier.wrapContentSize()
             )
         }
