15de25fab28ec1f30edf6878f32427aa63350999
StitchMl <matteo.lagioia@gmail.com>
2022-05-31 16:55:43 +0200

    Commit Sudoku

--- app/src/main/java/com/example/sudoku/screen/GameScreen.kt
+++ app/src/main/java/com/example/sudoku/screen/GameScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
+import com.example.sudoku.computation.NumberSelection
 import com.example.sudoku.computation.Sudoku
 import com.example.sudoku.model.Game
 
@@ -17,12 +18,17 @@ fun NewGameScreen(navController: NavHostController, empty: MutableState<Int>, di
     val g = Game(diff.value, s.get(), s.getSolution())
     ConstraintLayout {
         // Create references for the composable to constrain
-        val (sudoku) = createRefs()
+        val (sudoku, number) = createRefs()
 
         Box(modifier = Modifier.constrainAs(sudoku){
             top.linkTo(parent.top, margin = 8.dp)
         }) {
             CreateBoard(g.sudoku){ /*TODO*/ }
+        }
+        Box(modifier = Modifier.constrainAs(number){
+            top.linkTo(sudoku.bottom, margin = 10.dp)
+        }) {
+            NumberSelection{ /*TODO*/ }
         }
     }
 }
