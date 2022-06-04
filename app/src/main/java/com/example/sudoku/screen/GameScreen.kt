ffe71d534ce99ec11960ea712ce9c49a6a09f43e
StitchMl <matteo.lagioia@gmail.com>
2022-06-04 11:15:52 +0200

    Solve many problem

--- app/src/main/java/com/example/sudoku/screen/GameScreen.kt
+++ app/src/main/java/com/example/sudoku/screen/GameScreen.kt
@@ -3,22 +3,28 @@ package com.example.sudoku.screen
 import android.content.Context
 import android.widget.Toast
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.sudoku.R
-import com.example.sudoku.model.Cell
+import com.example.sudoku.computation.Sudoku
 import com.example.sudoku.model.Game
-import com.example.sudoku.model.NumberBar
+import com.example.sudoku.model.Setting
 
 @Composable
 fun NewGameScreen(
@@ -52,9 +58,21 @@ fun NewGameScreen(
 @Preview(device = Devices.DEFAULT, showBackground = true)
 @Composable
 fun NewGameScreenPreview(){
-    val counter = rememberSaveable { mutableStateOf(81) }
-    val diff = rememberSaveable { mutableStateOf("") }
-    NewGameScreen(rememberNavController(), Game(diff,
-        Array(9){ Array(9){ Cell(0,0,0,null, null) } },
-        NumberBar(), counter = counter), LocalContext.current)
+    val context = LocalContext.current
+    val set = Setting(context)
+    val k = rememberSaveable { mutableStateOf(0) }
+    val diff = rememberSaveable { mutableStateOf(set.DIFFICULTY[1]) }
+    set.setDifficult(diff.value, k)
+    val s = Sudoku(9, k, diff)
+    Column {
+        Text(
+            "${diff.value} ${k.value}",
+            style = TextStyle(
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Medium,
+                color = Color.Black
+            )
+        )
+        NewGameScreen(rememberNavController(), s.getGame(), context)
+    }
 }
