7cf146344022f10083904740907fda2aa06c649e
Matteo <92362045+StitchMl@users.noreply.github.com>
2022-06-20 10:02:07 +0200

    Resolve code smell

--- app/src/main/java/com/example/sudoku/database/GameRepository.kt
+++ app/src/main/java/com/example/sudoku/database/GameRepository.kt
@@ -2,30 +2,30 @@ package com.example.sudoku.database
 
 import androidx.lifecycle.LiveData
 import com.example.sudoku.model.Score
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class GameRepository(private val gameDao: GameDao) {
+class GameRepository(private val gameDao: GameDao, private val dispatcher: CoroutineDispatcher = Dispatchers.IO) {
 
     val allScore: LiveData<List<Score>> = gameDao.getAllScore()
     private val coroutineScope = CoroutineScope(Dispatchers.Main)
-    private val dis = Dispatchers.IO
 
     fun insertScore(newScore: Score) {
-        coroutineScope.launch(dis) {
+        coroutineScope.launch(dispatcher) {
             gameDao.insertScore(newScore)
         }
     }
 
     fun deleteScoreById(id: Int) {
-        coroutineScope.launch(dis) {
+        coroutineScope.launch(dispatcher) {
             gameDao.deleteScoreById(id)
         }
     }
 
     fun deleteScore(score: Score) {
-        coroutineScope.launch(dis) {
+        coroutineScope.launch(dispatcher) {
             gameDao.deleteScore(score)
         }
     }
